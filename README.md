9ef52768e90cb813ff612281bb6dfb0cab08a775
jamesnyakush <emvesttech@gmail.com>
2020-03-19 19:22:19 +0300

    sweet readme added

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-#TruckHub Backend
+# TruckHub Backend :truck:
+
+
+Truckhub Backend is a fully featured Api.
+
+## Api Endpoints
+| Endpoint | Method | Status |
+| :------: |:------:|:------:|
+| /register-url | Get | Used to register Urls |
+
+## Tools used
+
+* Used [SmoDav](https://github.com/SmoDav/mpesa) Mpesa Library to integrate with daraja.
+* Used [vue](https://vuejs.org/) For Frontend Development.
+* Used [AfricasTalking](https://github.com/AfricasTalkingLtd/africastalking-php) to send sms and notification.
+* Used [passport](https://laravel.com/docs/5.8/passport) to generate OAuth tokens.
+* Used [kreait](https://github.com/kreait/firebase-php) for Push Notification.
+
+
+## Inspiration
+
+
+## Contributors
+
+Made with [contributors-img](https://contributors-img.web.app).
+
+<a href="https://github.com/jamesnyakush/truckhub-server/graphs/contributors">
+  <img src="https://contributors-img.web.app/image?repo=jamesnyakush/truckhub-server" />
+</a>
+
+
+
+
--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,7 @@
 <?php
 
-Route::group(['prefix' => 'payment'] ,function(){
+Route::group(['prefix' => 'payment'], function () {
     //mpesa callback urls
-    // Route::get('/register-url', 'Api\v1\mpesa\MpesaController@c2bRegisterUrls');
-    // Route::post('/pay', 'Api\v1\mpesa\MpesaController@simulateC2B');
-
-    // Route::any('/validate', 'Api\v1\mpesa\CallbackController@validateTransaction');
-    // Route::any('/confirm', 'Api\v1\mpesa\CallbackController@confirmTransaction');
-
     Route::get('/register-url', 'Api\v1\mpesa\MpesaController@register');
     Route::any('/validate', 'Api\v1\mpesa\MpesaController@validateTransaction');
     Route::any('/confirm', 'Api\v1\mpesa\MpesaController@confirmTransaction');
@@ -16,24 +10,24 @@ Route::group(['prefix' => 'payment'] ,function(){
 });
 
 
-Route::group(['prefix' => 'v1'],function(){
+Route::group(['prefix' => 'v1'], function () {
     //booking routers
-    Route::get('/bookings','Api\v1\booking\BookingController@index');
-    Route::get('/bookings/{booking}','Api\v1\booking\BookingController@show');
-    Route::post('/bookings','Api\v1\booking\BookingController@store');
-    Route::put('/bookings/{booking}','Api\v1\booking\BookingController@update');
-    Route::delete('/bookings/{id}','Api\v1\booking\BookingController@destroy');
+    Route::get('/bookings', 'Api\v1\booking\BookingController@index');
+    Route::get('/bookings/{booking}', 'Api\v1\booking\BookingController@show');
+    Route::post('/bookings', 'Api\v1\booking\BookingController@store');
+    Route::put('/bookings/{booking}', 'Api\v1\booking\BookingController@update');
+    Route::delete('/bookings/{id}', 'Api\v1\booking\BookingController@destroy');
 
     //truck routers
-    Route::get('/trucks','Api\v1\truck\TruckController@index');
-    Route::get('/trucks/{id}','Api\v1\truck\TruckController@show');
-    Route::post('/trucks','Api\v1\truck\TruckController@store');
-    Route::put('/trucks/{id}','Api\v1\truck\TruckController@update');
-    Route::delete('/trucks/{id}','Api\v1\truck\TruckController@destroy');
+    Route::get('/trucks', 'Api\v1\truck\TruckController@index');
+    Route::get('/trucks/{id}', 'Api\v1\truck\TruckController@show');
+    Route::post('/trucks', 'Api\v1\truck\TruckController@store');
+    Route::put('/trucks/{id}', 'Api\v1\truck\TruckController@update');
+    Route::delete('/trucks/{id}', 'Api\v1\truck\TruckController@destroy');
 
     //payment routers
-    Route::get('/payments','Api\v1\payment\PaymentController@index');
-    Route::post('/payments','Api\v1\payment\PaymentController@store');
+    Route::get('/payments', 'Api\v1\payment\PaymentController@index');
+    Route::post('/payments', 'Api\v1\payment\PaymentController@store');
 });
 
 Route::group(['prefix' => 'v1/auth'], function () {
@@ -41,7 +35,7 @@ Route::group(['prefix' => 'v1/auth'], function () {
     Route::post('login', 'AuthController@login');
     Route::post('signup', 'AuthController@signup');
 
-    Route::group(['middleware' => 'auth:api'], function() {
+    Route::group(['middleware' => 'auth:api'], function () {
         Route::get('logout', 'AuthController@logout');
         Route::get('user', 'AuthController@user');
     });
